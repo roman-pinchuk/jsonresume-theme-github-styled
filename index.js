a53f151923a55def15c3953bc8815dbaa4eb07ac
roman-pinchuk <hi@thisroman.dev>
2026-04-30 18:40:18 +0300

    fix: round favicon in browser-safe way

--- index.js
+++ index.js
@@ -22,25 +22,6 @@ Handlebars.registerHelper('formatDate', function(dateString) {
   return dateString.substring(0, 4);
 });
 
-Handlebars.registerHelper('faviconDataUrl', function(imageUrl) {
-  if (!imageUrl) return '';
-
-  const escapedImageUrl = String(imageUrl)
-    .replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;');
-
-  const svg = [
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">',
-    '<defs><clipPath id="avatar"><circle cx="32" cy="32" r="32" /></clipPath></defs>',
-    '<image href="' + escapedImageUrl + '" width="64" height="64" preserveAspectRatio="xMidYMid slice" clip-path="url(#avatar)" />',
-    '</svg>'
-  ].join('');
-
-  return 'data:image/svg+xml,' + encodeURIComponent(svg);
-});
-
 function render(resume) {
   const templatePath = path.join(__dirname, 'resume.hbs');
   const template = fs.readFileSync(templatePath, 'utf-8');
